2994257999c9f9ac91d36afee1a08bf0ab8a6139
Kate Holdener <kate.holdener@slu.edu>
2021-11-04 13:45:49 -0500

    Added a /multiply route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import request
 from app.tools.sum import sum
+from app.tools.product import product
 
 @app.route('/')
 @app.route('/index')
@@ -13,6 +14,14 @@ def add():
     _list = list(map(int, data.split(',')))
     
     total = sum(_list)
+    return 'Result= ' + str(total)
+
+@app.route('/multiply')
+def multiply():
+    data = request.args.get('data', None)
+    _list = list(map(int, data.split(',')))
+    
+    total = product(_list)
     return 'Result= ' + str(total)
 
 
